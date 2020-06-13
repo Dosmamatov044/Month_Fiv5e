fdf90f827ffa2f726d14c65a4ebfe56bc89e9a29
Dosmamatov044 <dosmamatov24@gmail.com>
2020-06-14 01:05:33 +0600

    2

--- app/src/main/java/com/example/month_fiv5e/onBoard/IntroActivity.java
+++ app/src/main/java/com/example/month_fiv5e/onBoard/IntroActivity.java
@@ -1,17 +1,56 @@
 package com.example.month_fiv5e.onBoard;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 
 import com.example.month_fiv5e.R;
+import com.google.android.material.tabs.TabLayout;
+
 
 public class IntroActivity extends AppCompatActivity {
+    ViewPager viewPager;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_intro);
+        viewPager = findViewById(R.id.viewPager);
+        viewPager.setAdapter(new SectionsPagerAdapter(getSupportFragmentManager()));
+
+        TabLayout tabLayout = findViewById(R.id.tabLayout);
+        tabLayout.setupWithViewPager(viewPager, true);
+
+    }
+
+    public class SectionsPagerAdapter extends FragmentPagerAdapter {
+
+        public SectionsPagerAdapter(@NonNull FragmentManager fm) {
+            super(fm, FragmentPagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
+        }
+
+        @NonNull
+        @Override
+        public Fragment getItem(int position) {
+            Bundle bundle = new Bundle();
+            bundle.putInt("pos", position);
+            IntroFragment introFragment = new IntroFragment();
+            introFragment.setArguments(bundle);
+            return introFragment;
+        }
+
+        @Override
+        public int getCount() {
+            return 3;
+        }
     }
 }
+
+
+
---
+++ app/src/main/java/com/example/month_fiv5e/onBoard/IntroFragment.java
@@ -0,0 +1,29 @@
+package com.example.month_fiv5e.onBoard;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.month_fiv5e.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class IntroFragment extends Fragment {
+
+    public IntroFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_intro, container, false);
+    }
+}
