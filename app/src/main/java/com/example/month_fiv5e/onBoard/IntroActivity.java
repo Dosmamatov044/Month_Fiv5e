627f4a93e7cca2de179764567d9e1d83762e1feb
Dosmamatov044 <dosmamatov24@gmail.com>
2020-06-13 21:34:39 +0600

    1

---
+++ app/src/main/java/com/example/month_fiv5e/onBoard/IntroActivity.java
@@ -0,0 +1,17 @@
+package com.example.month_fiv5e.onBoard;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.month_fiv5e.R;
+
+public class IntroActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_intro);
+    }
+}
